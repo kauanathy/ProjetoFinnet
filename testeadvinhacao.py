cd129fb8ae34a6eb37e415018326d80b0b470ccf
kauanathy <143861467+kauanathy@users.noreply.github.com>
2024-09-27 05:38:18 -0300

    Delete testeadvinhacao.py

--- testeadvinhacao.py
+++
@@ -1,8 +0,0 @@
-velocidade = float(input("Qual foi a velocidade percorrida?"))
-velocidadeMaxima = 80.0
-multa = (velocidade-80) * 7
-
-if velocidade > velocidadeMaxima:
-    print("Você foi multado! Terá que pagar uma multa de {} reais".format(multa))
-else:
-    print("Ta deboas")
